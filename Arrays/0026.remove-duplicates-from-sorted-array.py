5a94477834f4f7f67c2fd3639f12138b4da1a4a1
navyanaidu chodisetti <cnavyanaidu@gmail.com>
2026-07-23 18:36:44 +0530

    Add removeDuplicates method to Solution class
    Implement removeDuplicates method to remove duplicates in-place from a sorted array.

--- Arrays/0026.remove-duplicates-from-sorted-array.py
+++ Arrays/0026.remove-duplicates-from-sorted-array.py
@@ -1,1 +1,11 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
 
+        write = 1
+        for read in range(1, len(nums)):
+            if nums[read] != nums[read - 1]:
+                nums[write] = nums[read]
+                write +=1
+        return write
